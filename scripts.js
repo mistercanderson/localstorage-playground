f05f738628e17b8083fa3791c426a74c0e0adb1d
Chris Anderson <mistercanderson@gmail.com>
2021-02-25 12:14:38 -0700

    Use local storage to display contacts

--- scripts.js
+++ scripts.js
@@ -5,13 +5,66 @@
 */
 
 // Query selectors
-
-
+var buttons = {
+  register: document.getElementById('register-btn'),
+  display: document.getElementById('display-btn')
+};
+var inputs = {
+  name: document.getElementById('name'),
+  email: document.getElementById('email'),
+};
+var containers = {
+  display: document.querySelector('.display-area'),
+  form: document.querySelector('form')
+}
+var contacts = [];
 
 // Event listeners
-
+containers.form.addEventListener('click', formFunctions);
+window.addEventListener('load', displayContacts);
 
 
 
 // Functions
+function formFunctions() {
+  addContact();
+  displayContacts();
+};
 
+function addContact() {
+  if (event.target === buttons.register) {
+    var contact = {
+      name: inputs.name.value,
+      email: inputs.email.value
+    };
+    contacts.push(contact);
+    localStorage.setItem(`${contact.name}`, JSON.stringify(contact));
+    displayContacts()
+  };
+};
+
+function displayContacts() {
+  if (event.target === buttons.display ||
+    event.currentTarget === window) {
+    contacts = [];
+    var keys = Object.keys(localStorage)
+    for (var i = 0; i < keys.length; i++) {
+      var contact = JSON.parse(localStorage.getItem(keys[i]));
+      contacts.push(contact)
+      };
+    };
+    renderContacts()
+  };
+
+
+function renderContacts() {
+  containers.display.innerHTML = '';
+  for (var i = 0; i < contacts.length; i++) {
+    containers.display.innerHTML += `
+    <div class="contact-card">
+    <h4>${contacts[i].name}</h4>
+    <h5>${contacts[i].email}</h5>
+    </div>
+    `
+  };
+};
